893f5ad0e2f7139f14251f2edb6747bb270cfab8
MD-ARIF-UDDIN <mdarifuddin040@gmail.comm>
2022-05-24 06:33:20 +0600

    auth added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React from "react";
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from "../../firebase.init";
 
 const Login = () => {
-    return (
-        <div>
-            <h1>this is login</h1>
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    if(user){
+        console.log(user);
+    }
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="card-body">
+          <h2 className="text-center text-4xl font-bold">Login</h2>
+          <div className="divider">OR</div>
+          <button onClick={() => signInWithGoogle()} className="btn btn-outline">Continue with google</button>
+          </div>
         </div>
-    );
+      </div>
+
+  );
 };
 
 export default Login;
--- src/Pages/MyPortfolio/MyPortfolio.js
+++ src/Pages/MyPortfolio/MyPortfolio.js
@@ -2,12 +2,14 @@ import React from "react";
 
 const MyPortfolio = () => {
   return (
-    <div class="card w-2/4 bg-base-100 shadow-xl">
-      <div class="card-body">
-        <h2 class="card-title">Md Arif Uddin</h2>
-        <h1>Email:mdarifuddin040@gmail.com</h1>
-        <h1 className="text-xl font-bold">Edcational Background:</h1>
-        <h1>Running:BSc in CSE,PORT CITY INTERNATIONAL UNIVERSITY</h1>
+    <div className="flex justify-center">
+      <div className="card  w-2/4  bg-base-100 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title">Md Arif Uddin</h2>
+          <h1>Email:mdarifuddin040@gmail.com</h1>
+          <h1 className="text-xl font-bold">Edcational Background:</h1>
+          <h1>Running:BSc in CSE,PORT CITY INTERNATIONAL UNIVERSITY</h1>
+        </div>
       </div>
     </div>
   );
