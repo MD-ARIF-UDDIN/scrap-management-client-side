7a9aaf92b7d64206402e9e2c00242176e7430e39
MD-ARIF-UDDIN <mdarifuddin040@gmail.comm>
2022-05-28 12:57:16 +0600

    added

--- src/hooks/usePurchases.js
+++ src/hooks/usePurchases.js
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 const usePurchases = () => {
   const [purchases, setPurchases] = useState([]);
   useEffect(() => {
-    fetch("https://tranquil-wave-41515.herokuapp.com")
+    fetch("https://tranquil-wave-41515.herokuapp.com/purchase")
       .then((res) => res.json())
       .then((data) => setPurchases(data));
   }, []);
