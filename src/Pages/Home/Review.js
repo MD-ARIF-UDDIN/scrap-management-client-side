3d6cfd2b7d9a9908cf16bdb2b34d78a2584242db
MD-ARIF-UDDIN <mdarifuddin040@gmail.comm>
2022-08-24 14:24:45 +0600

    latest

--- src/Pages/Home/Review.js
+++ src/Pages/Home/Review.js
@@ -10,7 +10,7 @@ const Review = ({reviews}) => {
         <img className="bg-red-200 mt-3 mb-3 w-24 h-24 rounded-full shadow-lg" src="https://imgs.search.brave.com/9JdKPC11-2QyaY-ExDGdO4OTIvjRyxVt36-3gQshi3Q/rs:fit:512:512:1/g:ce/aHR0cHM6Ly9jZG4w/Lmljb25maW5kZXIu/Y29tL2RhdGEvaWNv/bnMvYXZhdGFyLXZv/bC0zLTIvNTEyLzkt/NTEyLnBuZw" alt={customerName}/>
         <h5 className="mb-1 text-2xl font-bold text-primary">{customerName}</h5>
        
-        <span className="text-xl text-black ">"{reviewText}"</span>
+        <span className="text-xl text-black ">y"{reviewText}"</span>
         <span className=" text-xl text-black">Stars:{stars}</span>
     </div>
 </div>
